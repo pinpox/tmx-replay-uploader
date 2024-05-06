fcfa7820576b7cbd2f5cc02f176616cecd309548
Pablo Ovelleiro Corral <mail@pablo.tools>
2024-05-06 17:00:04 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,90 @@
+
+// Add event listener to the button element
+const uploadButton = document.querySelector('#uploadButton');
+uploadButton.addEventListener('click', uploadFiles);
+
+function uploadFiles(event) {
+	event.preventDefault();
+
+	const fileInput = document.querySelector('#fileInput');
+	const selectedFiles = fileInput.files;
+
+	const formData = new FormData();
+
+	for (const selectedFile of selectedFiles) {
+		// Console.log(selectedFiles[i]);
+		uploadReplay(selectedFile);
+	}
+}
+
+function userlog(text) {
+	document.querySelector('#userlog').innerHTML += text + '</br>';
+}
+
+function uploadReplay(file) {
+	const request = new XMLHttpRequest();
+	request.addEventListener('readystatechange', () => {
+		if (request.readyState === XMLHttpRequest.DONE) {
+			if (request.status === 200) {
+				// Handle successful response from the server
+				userlog(file.name + ' uploaded successfully!');
+			} else {
+				userlog(file.name + ' failed to upload!');
+			}
+		}
+	});
+
+	const url = 'https://trackmania.exchange/upload/replay';
+	// Const url='http://localhost:9999'
+
+	request.open('POST', url, true);
+	request.withCredentials = true;
+
+	request.setRequestHeader('User-Agent', 'Mozilla/5.0 (X11; Linux x86_64; rv:124.0) Gecko/20100101 Firefox/124.0');
+	request.setRequestHeader('Accept', 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8');
+	request.setRequestHeader('Accept-Language', 'en-US,en;q=0.5');
+	request.setRequestHeader('Accept-Encoding', 'gzip, deflate, br');
+	request.setRequestHeader('Content-Type', 'multipart/form-data; boundary=---------------------------86198276832215236822279235129');
+	request.setRequestHeader('Origin', 'https://trackmania.exchange');
+	request.setRequestHeader('DNT', '1');
+	request.setRequestHeader('Connection', 'keep-alive');
+	request.setRequestHeader('Referer', 'https://trackmania.exchange/upload/replay');
+	// Request.setRequestHeader('Upgrade-Insecure-Requests', '1');
+	// request.setRequestHeader('Sec-Fetch-Dest', 'document');
+	// request.setRequestHeader('Sec-Fetch-Mode', 'navigate');
+	// request.setRequestHeader('Sec-Fetch-Site', 'same-origin');
+	// request.setRequestHeader('Sec-Fetch-User', '?1');
+	request.setRequestHeader('Pragma', 'no-cache');
+	request.setRequestHeader('Cache-Control', 'no-cache');
+	request.setRequestHeader('TE', 'trailers');
+
+	request.setRequestHeader('Access-Control-Allow-Origin', '*');
+
+	const part1 = '-----------------------------86198276832215236822279235129\r\nContent-Disposition: form-data; name="replay_file"; filename="';
+
+	const part3 = '"\r\nContent-Type: application/octet-stream\r\n\r\n';
+	const part5 = '\r\n-----------------------------86198276832215236822279235129--\r\n';
+
+	const fr = new FileReader();
+	fr.addEventListener('load', () => {
+		const s1 = document.querySelector('#sessionid').value;
+		const s2 = document.querySelector('#mxclientauth').value;
+
+		document.cookie = 'ASP.NET_SessionId='.concat(s1, '; samesite=lax;');
+		document.cookie = '.mxclientauth='.concat(s2, ';samesite=lax');
+
+		const requestParts = [
+			part1,
+			file.name,
+			part3,
+			fr.result,
+			part5,
+		];
+
+		postData = ''.concat(...requestParts);
+
+		request.send(postData);
+	});
+
+	const gbxContent = fr.readAsBinaryString(file);
+}
